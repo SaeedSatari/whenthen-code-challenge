49f4209a52e2b152bb86666e1d8bf80f71225398
SaeedSatari <saeedsatari93@gmail.com>
2021-01-23 20:52:49 +0330

    added swagger

--- src/main/java/com/whenthen/challenge/controller/TicketController.java
+++ src/main/java/com/whenthen/challenge/controller/TicketController.java
@@ -5,11 +5,9 @@ import com.whenthen.challenge.model.CommentModel;
 import com.whenthen.challenge.model.RequestModel;
 import com.whenthen.challenge.model.TicketModel;
 import com.whenthen.challenge.model.request.TicketRequest;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,19 +17,23 @@ import java.util.Random;
 @RestController
 @CrossOrigin
 @AllArgsConstructor
+@RequestMapping("api/tickets")
 public class TicketController {
 
     private final RestClient restClient;
 
-    @PostMapping(path = "/tickets")
-    public Map<String, Long> createSupportTicket(@RequestBody TicketRequest request) {
+    @PostMapping
+    @ApiOperation(value = "create a support ticket and call zendesk api to get list of the messages and count priorities"
+            , nickname = "create-support-ticket-count-priorities")
+    public Map<String, Long> createSupportTicketAndCountPriorities(@RequestBody TicketRequest request) {
         CommentModel commentModel = CommentModel.builder().body(request.getMessage()).build();
         TicketModel ticketModel = TicketModel.builder().priority(getRandomPriority()).subject(request.getSubject()).comment(commentModel).build();
         RequestModel requestModel = RequestModel.builder().ticket(ticketModel).build();
         return restClient.createTicketAndCountPriorities(requestModel);
     }
 
-    private String getRandomPriority(){
+    // I created this method, because I did not know how to set priority field from given request
+    private String getRandomPriority() {
         List<String> priorities = new ArrayList<>();
         priorities.add("urgent");
         priorities.add("high");
--- src/main/java/com/whenthen/challenge/model/request/TicketRequest.java
+++ src/main/java/com/whenthen/challenge/model/request/TicketRequest.java
@@ -1,12 +1,20 @@
 package com.whenthen.challenge.model.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class TicketRequest {
 
+    @ApiModelProperty(example = "Saeed Sattari", required = true)
     private String name;
+
+    @ApiModelProperty(example = "saeedsatari93@gmail.com", required = true)
     private String email;
+
+    @ApiModelProperty(example = "Test subject", required = true)
     private String subject;
+
+    @ApiModelProperty(example = "Test message from client", required = true)
     private String message;
 }
