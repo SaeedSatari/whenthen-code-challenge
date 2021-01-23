46ee6cdcc7117dbf21eb2b25c1d86aec7ebef470
SaeedSatari <saeedsatari93@gmail.com>
2021-01-23 09:46:08 +0330

    extracted rest template configuration

--- src/main/java/com/whenthen/challenge/WhenThenCodeChallengeApplication.java
+++ src/main/java/com/whenthen/challenge/WhenThenCodeChallengeApplication.java
@@ -2,18 +2,11 @@ package com.whenthen.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WhenThenCodeChallengeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WhenThenCodeChallengeApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
     }
 }
